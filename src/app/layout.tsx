93fe97603682eec2985f7bd90a0c8187ab042726
Filao-o <lcscty97419@gmail.com>
2026-08-10 01:18:32 +0400

    Add Google Ads conversion tracking on form submission with dynamic price value

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,6 +112,14 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`h-full antialiased ${cormorant.variable} ${inter.variable}`}>
       <head>
+        {/* Google Ads tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11120101808" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11120101808');
+        `}} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
